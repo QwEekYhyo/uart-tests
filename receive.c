218727ef3db47fdb8f80ce12f5a15b6639396de3
Logan Lucas <logan.lucas83@gmail.com>
2024-12-29 11:18:05 +0100

    add some comments about the UART configuration

--- receive.c
+++ receive.c
@@ -12,19 +12,20 @@ int main(void) {
         return 1;
     }
     
+    // UART configuration
     struct termios options;
     tcgetattr(fd, &options);
-    cfsetispeed(&options, B115200);
-    cfsetospeed(&options, B115200);
-    options.c_cflag |= (CLOCAL | CREAD);
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag |= CS8;
-    options.c_cflag &= ~PARENB;
-    options.c_cflag &= ~CSTOPB;
-    options.c_cflag &= ~CRTSCTS;
-    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-    options.c_iflag &= ~(IXON | IXOFF | IXANY);
-    options.c_oflag &= ~OPOST;
+    cfsetispeed(&options, B115200); // set input baudrate
+    cfsetospeed(&options, B115200); // set output baudrate, unused for now
+    options.c_cflag |= (CLOCAL | CREAD); // ignore modem signals and enable RX
+    options.c_cflag &= ~CSIZE; // clear data bit size mask
+    options.c_cflag |= CS8; // set word length to 8
+    options.c_cflag &= ~PARENB; // no parity bit
+    options.c_cflag &= ~CSTOPB; // only 1 stop bit
+    options.c_cflag &= ~CRTSCTS; // disable hardware flow control
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // input handling
+    options.c_iflag &= ~(IXON | IXOFF | IXANY); // disable software flow control
+    options.c_oflag &= ~OPOST; // disable output post-processing, unused for now
     tcsetattr(fd, TCSANOW, &options);
 
     fcntl(fd, F_SETFL, FNDELAY);
